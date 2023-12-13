c7defab74910f217069b59cfb49ca6e090761590
Fatma Ezgi ERKAT <f.ezgierkatt@gmail.com>
2023-12-13 20:52:40 +0300

    Create MainApplicationa.java
    MainApplicationa class created

---
+++ src/main/java/com/example/projemizinilkguitasarimi/MainApplicationa.java
@@ -0,0 +1,15 @@
+package com.example.projemizinilkguitasarimi;
+
+import com.google.gson.Gson;
+
+import java.util.Scanner;
+
+public class MainApplicationa {
+    Gson gson = new Gson();
+    String filePath = code + ".json";
+
+
+
+
+
+}
