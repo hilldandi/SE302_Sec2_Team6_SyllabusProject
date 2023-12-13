6f3f8c4241cc4ee1ba41c350f678e96912aaf6e8
NİLAY GÜLER <gulernilay088@gmail.com>
2023-12-13 21:57:47 +0300

    SwitchtoScene functions for MainControllerX class is added

--- src/main/java/com/example/projemizinilkguitasarimi/MainControllerX.java
+++ src/main/java/com/example/projemizinilkguitasarimi/MainControllerX.java
@@ -1,15 +1,35 @@
 package com.example.projemizinilkguitasarimi;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MainControllerX {
 
+    public void switchToScene1(ActionEvent event ) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage=((Stage) ( (Node)event.getSource()).getScene().getWindow());
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene2(ActionEvent event ) throws IOException {
+        Parent root=FXMLLoader.load(getClass().getResource("start.fxml"));
+        stage=((Stage) ( (Node)event.getSource()).getScene().getWindow());
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /* they add i main
         public static void main(String[] args) {
@@ -371,6 +391,7 @@ public class MainControllerX {
         course.setReasonForUpdate(SaveAllText.getText());
 
     }
+
 
 
 
