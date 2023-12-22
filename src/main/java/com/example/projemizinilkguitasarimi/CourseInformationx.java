91120c065be450927128eb9b3d6af98847921742
Fatma Ezgi ERKAT <f.ezgierkatt@gmail.com>
2023-12-22 19:17:07 +0300

    CourseInformationx and MainControllerX created

---
+++ src/main/java/com/example/projemizinilkguitasarimi/CourseInformationx.java
@@ -0,0 +1,11 @@
+package com.example.projemizinilkguitasarimi;
+
+import com.google.gson.Gson;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CourseInformationx {
+}
---
+++ src/main/java/com/example/projemizinilkguitasarimi/MainControllerX.java
@@ -0,0 +1,1026 @@
+package com.example.projemizinilkguitasarimi;
+
+import com.google.gson.Gson;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
+
+
+public class MainControllerX {
+    @FXML
+    private Label file1;
+    @FXML
+    private Label file2;
+
+    @FXML
+    private Label warning;
+
+
+    @FXML
+    private javafx.scene.control.ListView<String> SaveTab;
+
+    @FXML
+    private javafx.scene.control.ListView<String> Tab1list = new ListView<>();
+
+    @FXML
+    private javafx.scene.control.ListView<String> Tab2list = new ListView<>();
+
+    @FXML
+    private javafx.scene.control.ListView<String> Tab3list= new ListView<>();
+
+    @FXML
+    private javafx.scene.control.ListView<String> Tab4list= new ListView<>();
+
+    @FXML
+    private javafx.scene.control.ListView<String> Tab5list= new ListView<>();
+
+    @FXML
+    private javafx.scene.control.ListView<String> Tab6list= new ListView<>();
+
+    @FXML
+    private javafx.scene.control.ListView<String> ListView = new ListView<>();
+    @FXML
+    private ComboBox<String> comboBox = new ComboBox<>();
+    static String[] Keys = new String[100];
+    static String[] values = new String[100];
+    static HashMap fileMap;
+
+
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private TextField CourseLanguageText;
+
+    @FXML
+    private TextField CourseLevelText;
+
+    @FXML
+    private TextArea CoursePOMText1;
+
+    @FXML
+    private TextArea CoursePOMText10;
+
+    @FXML
+    private TextField CoursePOMText101;
+
+    @FXML
+    private TextField CoursePOMText102;
+
+    @FXML
+    private TextField CoursePOMText103;
+
+    @FXML
+    private TextField CoursePOMText104;
+
+    @FXML
+    private TextField CoursePOMText105;
+
+    @FXML
+    private TextField CoursePOMText10LO;
+
+    @FXML
+    private TextField CoursePOMText11;
+
+    @FXML
+    private TextArea CoursePOMText110;
+
+    @FXML
+    private TextField CoursePOMText111;
+
+    @FXML
+    private TextField CoursePOMText112;
+
+    @FXML
+    private TextField CoursePOMText113;
+
+    @FXML
+    private TextField CoursePOMText114;
+
+    @FXML
+    private TextField CoursePOMText115;
+
+    @FXML
+    private TextField CoursePOMText11LO;
+
+    @FXML
+    private TextField CoursePOMText12;
+
+    @FXML
+    private TextArea CoursePOMText120;
+
+    @FXML
+    private TextField CoursePOMText121;
+
+    @FXML
+    private TextField CoursePOMText122;
+
+    @FXML
+    private TextField CoursePOMText123;
+
+    @FXML
+    private TextField CoursePOMText124;
+
+    @FXML
+    private TextField CoursePOMText125;
+
+    @FXML
+    private TextField CoursePOMText12LO;
+
+    @FXML
+    private TextField CoursePOMText13;
+
+    @FXML
+    private TextArea CoursePOMText130;
+
+    @FXML
+    private TextField CoursePOMText131;
+
+    @FXML
+    private TextField CoursePOMText132;
+
+    @FXML
+    private TextField CoursePOMText133;
+
+    @FXML
+    private TextField CoursePOMText134;
+
+    @FXML
+    private TextField CoursePOMText135;
+
+    @FXML
+    private TextField CoursePOMText13LO;
+
+    @FXML
+    private TextField CoursePOMText14;
+
+    @FXML
+    private TextField CoursePOMText15;
+
+    @FXML
+    private TextField CoursePOMText1LO;
+
+    @FXML
+    private TextArea CoursePOMText2;
+
+    @FXML
+    private TextField CoursePOMText21;
+
+    @FXML
+    private TextField CoursePOMText22;
+
+    @FXML
+    private TextField CoursePOMText23;
+
+    @FXML
+    private TextField CoursePOMText24;
+
+    @FXML
+    private TextField CoursePOMText25;
+
+    @FXML
+    private TextField CoursePOMText2LO;
+
+    @FXML
+    private TextArea CoursePOMText3;
+
+    @FXML
+    private TextField CoursePOMText31;
+
+    @FXML
+    private TextField CoursePOMText32;
+
+    @FXML
+    private TextField CoursePOMText32LO;
+
+    @FXML
+    private TextField CoursePOMText33;
+
+    @FXML
+    private TextField CoursePOMText34;
+
+    @FXML
+    private TextField CoursePOMText35;
+
+    @FXML
+    private TextArea CoursePOMText4;
+
+    @FXML
+    private TextField CoursePOMText41;
+
+    @FXML
+    private TextField CoursePOMText42;
+
+    @FXML
+    private TextField CoursePOMText43;
+
+    @FXML
+    private TextField CoursePOMText44;
+
+    @FXML
+    private TextField CoursePOMText45;
+
+    @FXML
+    private TextField CoursePOMText4LO;
+
+    @FXML
+    private TextArea CoursePOMText5;
+
+    @FXML
+    private TextField CoursePOMText51;
+
+    @FXML
+    private TextField CoursePOMText52;
+
+    @FXML
+    private TextField CoursePOMText53;
+
+    @FXML
+    private TextField CoursePOMText54;
+
+    @FXML
+    private TextField CoursePOMText55;
+
+    @FXML
+    private TextField CoursePOMText5LO;
+
+    @FXML
+    private TextArea CoursePOMText6;
+
+    @FXML
+    private TextField CoursePOMText61;
+
+    @FXML
+    private TextField CoursePOMText62;
+
+    @FXML
+    private TextField CoursePOMText63;
+
+    @FXML
+    private TextField CoursePOMText64;
+
+    @FXML
+    private TextField CoursePOMText65;
+
+    @FXML
+    private TextField CoursePOMText6LO;
+
+    @FXML
+    private TextArea CoursePOMText7;
+
+    @FXML
+    private TextField CoursePOMText71;
+
+    @FXML
+    private TextField CoursePOMText72;
+
+    @FXML
+    private TextField CoursePOMText73;
+
+    @FXML
+    private TextField CoursePOMText74;
+
+    @FXML
+    private TextField CoursePOMText75;
+
+    @FXML
+    private TextField CoursePOMText7LO;
+
+    @FXML
+    private TextArea CoursePOMText8;
+
+    @FXML
+    private TextField CoursePOMText81;
+
+    @FXML
+    private TextField CoursePOMText82;
+
+    @FXML
+    private TextField CoursePOMText83;
+
+    @FXML
+    private TextField CoursePOMText84;
+
+    @FXML
+    private TextField CoursePOMText85;
+
+    @FXML
+    private TextField CoursePOMText8LO;
+
+    @FXML
+    private TextArea CoursePOMText9;
+
+    @FXML
+    private TextField CoursePOMText91;
+
+    @FXML
+    private TextField CoursePOMText92;
+
+    @FXML
+    private TextField CoursePOMText93;
+
+    @FXML
+    private TextField CoursePOMText94;
+
+    @FXML
+    private TextField CoursePOMText95;
+
+    @FXML
+    private TextField CoursePOMText9LO;
+
+    @FXML
+    private TextField CourseTypeText;
+
+
+    @FXML
+    private TextField ModeOfDeleveryText;
+
+
+    @FXML
+    private TextArea ProjectNumText;
+
+    @FXML
+    private TextArea SaveAllText;
+
+
+    @FXML
+    private TextField applicationText2;
+
+
+    @FXML
+    private TextField assistantText;
+
+    @FXML
+    private TextField cdText;
+
+
+    @FXML
+    private TextField codeTextt2;
+
+
+    @FXML
+    private TextField communicationandManagementSkillsCourseText;
+
+
+    @FXML
+    private TextField coreCourseText;
+
+
+    @FXML
+    private TextField courseNameText2;
+
+
+    @FXML
+    private TextArea courseNotesText;
+
+
+    @FXML
+    private TextField coursecoordinatorText;
+
+
+    @FXML
+    private TextArea coursedescriptionText;
+
+
+    @FXML
+    private TextField courselecturerText;
+
+
+    @FXML
+    private TextArea courseobjectivesText;
+
+    @FXML
+    private TextField cwText;
+
+
+    @FXML
+    private TextField ectsText2;
+
+
+    @FXML
+    private Button engButton;
+
+
+    @FXML
+    private TextField exdText;
+
+    @FXML
+    private TextField exnText;
+
+    @FXML
+    private TextField exwText;
+
+    @FXML
+    private TextField fdText;
+
+
+    @FXML
+    private TextArea fieldNumText;
+
+
+    @FXML
+    private TextArea fieldWText;
+
+    @FXML
+    private TextArea fieldo1Text;
+
+    @FXML
+    private TextArea fieldo2Text;
+
+    @FXML
+    private TextArea fieldo3Text;
+
+    @FXML
+    private TextArea fieldo4Text;
+
+    @FXML
+    private TextArea fieldo5Text;
+
+    @FXML
+    private TextArea fieldo6Text;
+
+    @FXML
+    private TextArea fieldo7Text;
+
+    @FXML
+    private TextField finalEGrade1;
+
+    @FXML
+    private TextField finalEGrade2;
+
+
+    @FXML
+    private TextArea finalNumText;
+
+    @FXML
+    private TextArea finalWText;
+
+    @FXML
+    private TextField finalgrade1;
+
+    @FXML
+    private TextField finalgrade2;
+
+
+    @FXML
+    private TextArea finalo1Text;
+
+    @FXML
+    private TextArea finalo2Text;
+
+    @FXML
+    private TextArea finalo3Text;
+
+    @FXML
+    private TextArea finalo4Text;
+
+    @FXML
+    private TextArea finalo5Text;
+
+    @FXML
+    private TextArea finalo6Text;
+
+    @FXML
+    private TextArea finalo7Text;
+
+    @FXML
+    private TextField fnText;
+
+    @FXML
+    private TextField fwText;
+
+
+    @FXML
+    private TextField hdText;
+
+    @FXML
+    private TextField hnText;
+
+
+    @FXML
+    private TextArea homeworkNumText;
+
+
+    @FXML
+    private TextArea homeworkWText;
+
+    @FXML
+    private TextArea homeworko1Text;
+
+    @FXML
+    private TextArea homeworko2Text;
+
+    @FXML
+    private TextArea homeworko3Text;
+
+    @FXML
+    private TextArea homeworko4Text;
+
+    @FXML
+    private TextArea homeworko5Text;
+
+    @FXML
+    private TextArea homeworko6Text;
+
+    @FXML
+    private TextArea homeworko7Text;
+
+
+    @FXML
+    private TextField hwText;
+
+
+    @FXML
+    private TextArea juryNumText;
+
+    @FXML
+    private TextArea juryWText;
+
+    @FXML
+    private TextArea juryo1Text;
+
+    @FXML
+    private TextArea juryo2Text;
+
+    @FXML
+    private TextArea juryo3Text;
+
+    @FXML
+    private TextArea juryo4Text;
+
+    @FXML
+    private TextArea juryo5Text;
+
+    @FXML
+    private TextArea juryo6Text;
+
+    @FXML
+    private TextArea juryo7Text;
+
+
+    @FXML
+    private TextArea labNumText;
+
+
+    @FXML
+    private TextArea labWText;
+
+    @FXML
+    private TextArea labo1Text;
+
+    @FXML
+    private TextArea labo2Text;
+
+    @FXML
+    private TextArea labo3Text;
+
+    @FXML
+    private TextArea labo4Text;
+
+    @FXML
+    private TextArea labo5Text;
+
+    @FXML
+    private TextArea labo6Text;
+
+    @FXML
+    private TextArea labo7Text;
+
+    @FXML
+    private TextField ldText;
+
+
+    @FXML
+    private TextArea learningoutcomesText;
+
+
+    @FXML
+    private TextField localCreditsText2;
+
+    @FXML
+    private TextField lwText;
+
+
+    @FXML
+    private TextField majorAreaCourseText;
+
+
+    @FXML
+    private TextField middText;
+
+    @FXML
+    private TextField midnText;
+
+
+    @FXML
+    private TextArea midtermNum;
+
+    @FXML
+    private TextArea midtermW;
+
+    @FXML
+    private TextArea midtermo1Text;
+
+    @FXML
+    private TextArea midtermo2Text;
+
+    @FXML
+    private TextArea midtermo3Text;
+
+    @FXML
+    private TextArea midtermo4Text;
+
+    @FXML
+    private TextArea midtermo5Text;
+
+    @FXML
+    private TextArea midtermo6Text;
+
+    @FXML
+    private TextArea midtermo7Text;
+
+    @FXML
+    private TextField midwText;
+
+
+    @FXML
+    private TextArea oralNumText;
+
+
+    @FXML
+    private TextArea oralWText;
+
+    @FXML
+    private TextArea oralo1Text;
+
+    @FXML
+    private TextArea oralo2Text;
+
+    @FXML
+    private TextArea oralo3Text;
+
+    @FXML
+    private TextArea oralo4Text;
+
+    @FXML
+    private TextArea oralo5Text;
+
+    @FXML
+    private TextArea oralo6Text;
+
+    @FXML
+    private TextArea oralo7Text;
+
+    @FXML
+    private TextField ordText;
+
+    @FXML
+    private TextField ornText;
+
+    @FXML
+    private TextField orwText;
+
+
+    @FXML
+    private TextArea participateNumText;
+
+    @FXML
+    private TextArea participateWText;
+
+    @FXML
+    private TextArea participateo1Text;
+
+    @FXML
+    private TextArea participateo2Text;
+
+    @FXML
+    private TextArea participateo3Text;
+
+    @FXML
+    private TextArea participateo4Text;
+
+    @FXML
+    private TextArea participateo5Text;
+
+    @FXML
+    private TextArea participateo6Text;
+
+    @FXML
+    private TextArea participateo7Text;
+
+
+    @FXML
+    private TextField portdText11;
+
+    @FXML
+    private TextField portnText;
+
+    @FXML
+    private TextField portwText11;
+
+    @FXML
+    private TextField prdText;
+
+
+    @FXML
+    private TextField prerequisitesText;
+
+
+    @FXML
+    private TextField prnText;
+
+    @FXML
+    private TextField prodText;
+
+
+    @FXML
+    private TextArea projectWText;
+
+    @FXML
+    private TextArea projecto1Text;
+
+    @FXML
+    private TextArea projecto2Text;
+
+    @FXML
+    private TextArea projecto3Text;
+
+    @FXML
+    private TextArea projecto4Text;
+
+    @FXML
+    private TextArea projecto5Text;
+
+    @FXML
+    private TextArea projecto6Text;
+
+    @FXML
+    private TextArea projecto7Text;
+
+    @FXML
+    private TextField pronText;
+
+    @FXML
+    private TextField prowText;
+
+    @FXML
+    private TextField prwText;
+
+    @FXML
+    private TextField qdText1;
+
+    @FXML
+    private TextField qnText1;
+
+    @FXML
+    private TextArea quiz7Text;
+
+
+    @FXML
+    private TextArea quizNumText;
+
+
+    @FXML
+    private TextArea quizWText;
+
+    @FXML
+    private TextArea quizo1Text;
+
+    @FXML
+    private TextArea quizo2Text;
+
+    @FXML
+    private TextArea quizo3Text;
+
+    @FXML
+    private TextArea quizo4Text;
+
+    @FXML
+    private TextArea quizo5Text;
+
+    @FXML
+    private TextArea quizo6Text;
+
+    @FXML
+    private TextField qwText;
+
+    @FXML
+    private TextArea reqMaterial1;
+
+    @FXML
+    private TextArea reqMaterial101;
+
+    @FXML
+    private TextArea reqMaterial11;
+
+    @FXML
+    private TextArea reqMaterial111;
+
+    @FXML
+    private TextArea reqMaterial121;
+
+    @FXML
+    private TextArea reqMaterial131;
+
+    @FXML
+    private TextArea reqMaterial141;
+
+    @FXML
+    private TextArea reqMaterial21;
+
+    @FXML
+    private TextArea reqMaterial31;
+
+    @FXML
+    private TextArea reqMaterial41;
+
+    @FXML
+    private TextArea reqMaterial51;
+
+    @FXML
+    private TextArea reqMaterial61;
+
+    @FXML
+    private TextArea reqMaterial71;
+
+    @FXML
+    private TextArea reqMaterial81;
+
+    @FXML
+    private TextArea reqMaterial91;
+
+    @FXML
+    private TextField sdText;
+
+
+    @FXML
+    private TextField seasontext;
+
+    @FXML
+    private TextField semdText;
+
+
+    @FXML
+    private TextArea seminarNumText;
+
+
+    @FXML
+    private TextArea seminarWText;
+
+    @FXML
+    private TextArea seminaro1Text;
+
+    @FXML
+    private TextArea seminaro2Text;
+
+    @FXML
+    private TextArea seminaro3Text;
+
+    @FXML
+    private TextArea seminaro4Text;
+
+    @FXML
+    private TextArea seminaro5Text;
+
+    @FXML
+    private TextArea seminaro6Text;
+
+    @FXML
+    private TextArea seminaro7Text;
+
+    @FXML
+    private TextField semnText;
+
+    @FXML
+    private TextField semwText;
+
+    @FXML
+    private TextField snText;
+
+
+    @FXML
+    private TextArea subject1;
+
+    @FXML
+    private TextArea subject101;
+
+    @FXML
+    private TextArea subject11;
+
+    @FXML
+    private TextArea subject111;
+
+    @FXML
+    private TextArea subject121;
+
+    @FXML
+    private TextArea subject131;
+
+    @FXML
+    private TextArea subject141;
+
+    @FXML
+    private TextArea subject21;
+
+    @FXML
+    private TextArea subject31;
+
+    @FXML
+    private TextArea subject41;
+
+    @FXML
+    private TextArea subject51;
+
+    @FXML
+    private TextArea subject61;
+
+    @FXML
+    private TextArea subject71;
+
+    @FXML
+    private TextArea subject81;
+
+    @FXML
+    private TextArea subject91;
+
+
+    @FXML
+    private TextArea suggestedReadingsText;
+
+
+    @FXML
+    private TextField supportiveCourseText;
+
+    @FXML
+    private TextField swText;
+
+
+    @FXML
+    private TextArea teachingMethodsText;
+
+
+    @FXML
+    private TextField theoryText2;
+
+    @FXML
+    private TextField totalG1;
+
+    @FXML
+    private TextField totalG2;
+
+
+    @FXML
+    private TextArea totalNumText;
+
+
+    @FXML
+    private TextArea totalWWText;
+
+    @FXML
+    private TextField totallWText;
+
+    @FXML
+    private TextArea totalo1Text;
+
+    @FXML
+    private TextArea totalo2Text;
+
+    @FXML
+    private TextArea totalo3Text;
+
+    @FXML
+    private TextArea totalo4Text;
+
+    @FXML
+    private TextArea totalo5Text;
+
+    @FXML
+    private TextArea totalo6Text;
+
+    @FXML
+    private TextArea totalo7Text;
+
+    @FXML
+    private Button trButton;
+
+
+    @FXML
+    private TextField transferableSkillCourseText;
+
+}
